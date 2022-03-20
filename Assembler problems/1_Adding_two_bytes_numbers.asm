1993badfda4b525a39f8a91478d11c912d46d79f
jbarciv <chemabc@gmail.com>
2022-03-20 10:01:05 +0100

    exercise 1

---
+++ Assembler problems/1_Adding_two_bytes_numbers.asm
@@ -0,0 +1,21 @@
+;**************************************************************
+;*  Academic Exercise 1
+;*  Author: Josep Barbera
+;*  
+;*  A code that adds two two-byte numbers placed in memory 
+;*  locations SumA and SumB and leaves the result in 
+;*  memory locations Res (3 bytes). 
+;**************************************************************
+
+Adding:
+    LDS r16, SumA       ; lower part
+    LDS r17, SumB
+    ADD r16, r17
+    STS Res, r16
+    LDS r16, SumA + 1   ; upper part
+    LDS r17, SumB + 1
+    ADC r16, r17
+    STS Res + 1, r16
+    LDI r16, 0          
+    ADC r16, r16        ; for carry saving
+    STS Res + 2, r16
