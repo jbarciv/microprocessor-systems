90849cacaa3632f1b1c3c53cb00f68bdafb62f04
jbarciv <chemabc@gmail.com>
2022-03-28 17:55:09 +0200

    lab_2_3 added

---
+++ C/Lab_2_3_blinking_via_interruption_with_callback/Lab_2_3_blinking_via_interruption.c
@@ -0,0 +1,37 @@
+ /***************************************************************
+* 	Lab practice 2
+*  	Exercise 3
+*  	Author: Josep Barbera
+*  	Date: 22 March 2022
+*
+*	Change the state of the LEDs periodically (every 0.5 s  
+*	approximately) using an external library and callbacks.
+****************************************************************/
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include "Int01s.h"
+
+unsigned char c;
+void change()
+{
+	c++;
+	if(c==5)
+	{
+		PORTC = ~PINC;
+		c=0;
+	}
+}
+
+int main(void)
+{
+	cli();
+	DDRC = 0xFF;
+    setupPeriodicInt();
+	setPeriodicIntCallback(change);
+	sei();
+    while (1)
+    {
+        /*whatever*/
+    }
+}
